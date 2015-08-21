bf354d8ccaa4959e2b8d1c8b0ae0137ba0d77689
David Siaw (via Travis CI) <david.siaw@mobingi.com>
2015-08-21 15:00:17 +0900

    rearrange

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 echo "installing" > /var/log/container_status
 
-bash /tmp/init/init.sh > /var/log/startup.log 2>&1
+bash /tmp/init/init.sh 2>&1 >> /var/log/startup.log 
 
 echo "complete" > /var/log/container_status
 
